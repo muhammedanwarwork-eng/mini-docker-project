8b6bb4d370eeb16cc939c33cf8518585223b1211
Muhammed Anwar <muhammedanwarwork@gmail.com>
2026-02-10 16:19:45 +0200

    Initial commit - Mini Project Docker Compose

---
+++ app/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+// DB connection
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+});
+
+app.get('/', async (req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  res.send(`Hello from Node.js! DB time: ${result.rows[0].now}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get('/hello', (req, res) => res.send("Hello from Mini Project!"));
